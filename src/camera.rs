b3a10a15db40c374c119f470990c9b6549033b64
Abucia <arthurbucia@gmail.com>
2026-03-05 23:46:29 +0100

    fix typo in file name

--- src/camera.rs
+++ src/camera.rs
@@ -1,9 +1,10 @@
 use glfw::ffi::glfwGetTime;
 
-use crate::math::{mat4::{self, Mat4}, vec3::Vec3};
+use crate::math::{mat4::Mat4, vec3::Vec3};
 
 
-
+// ignore unused for now
+#[allow(unused)]
 pub struct Camera {
     position: Vec3,
     orientation: Vec3,
@@ -21,7 +22,7 @@ impl Camera {
         }
     }
 
-    pub fn updateBasicRot(&mut self) {
+    pub fn update_basic_rot(&mut self) {
         let camera_target = Vec3::new(0.0, 0.0, 0.0);
         let camera_direction = self.position.sub(&camera_target).normalize();
 
@@ -42,6 +43,11 @@ impl Camera {
         
         let view: Mat4 = Mat4::look_at(Vec3 { x: cam_x, y: 0.0, z: cam_z }, camera_target, camera_up);
 
+        println!("Camera position: {:?}", self.position);
+        println!("Camera orientation: {:?}", self.orientation);
+        println!("Camera view matrix: {:?}", view);
+
+        // apply the view matrix to the viewport
     }
 
     // pub fn set_aspect_ratio(&mut self, aspect_ratio: f32) {
